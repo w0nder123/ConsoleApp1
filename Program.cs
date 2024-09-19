edb75f79be40cf984c685651050093b8bbd96af7
w0nder123 <sebastien.prejza@educhem.cz>
2024-09-19 07:44:30 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = "123";
+try
+{
+    int result = Int32.Parse(input);
+    Console.WriteLine(result);
+}
+catch(FormatException) 
+{
+    Console.WriteLine($"Unable to parse {input}");
+}
+try
+{
+    if (input is float)
+    {
+        Console.WriteLine("True");
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
+}
+catch 
+{
+    return;
+}
+
+float vystup;
+Console.WriteLine("Napište první čislo");
+float jedna = float.Parse(Console.ReadLine());
+Console.WriteLine("Napište druhé čislo");
+float dva = float.Parse(Console.ReadLine());
+vystup = jedna + dva;
+Console.WriteLine(vystup);  
